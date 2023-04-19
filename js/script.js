90ca042d3e004544719b0406ec4d3af7c1134161
Adrian Osak <osak.adrian.k@gmail.com>
2023-04-19 17:16:39 +0200

    updateDisplay and handling buttons functions added

--- js/script.js
+++ js/script.js
@@ -5,4 +5,36 @@
 
   const clearButton = document.getElementById("clear");
   clearButton.addEventListener("click", clear);
+
+  let expression = "";
+
+  handleNumberButtons = (value) => {
+    expression += value;
+    updateDisplay(expression);
+  };
+
+  handleFunctionButtons = (value) => {
+    expression += ` ${value} `;
+    updateDisplay(expression);
+    console.log(expression);
+  };
+
+  updateDisplay = (expression) => {
+    const display = document.getElementById("result");
+    display.textContent = expression;
+  };
+
+  const numbers = document.querySelectorAll(".button_number");
+  numbers.forEach(number => {
+    number.addEventListener('click', () => {
+      handleNumberButtons(number.textContent);
+    });
+  });
+
+  const operations = document.querySelectorAll(".button_function");
+  operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+      handleFunctionButtons(operation.textContent);
+    });
+  });
 }
